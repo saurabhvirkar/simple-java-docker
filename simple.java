408c34522c8fd505a04c36b90e9fe530d922149e
Saurabh Virkar <88393585+saurabhvirkar@users.noreply.github.com>
2024-08-30 18:39:20 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,11 +1,13 @@
 // This is a simple Java program.
 // FileName : "HelloWorld.java".
+import java.util.Date;
 
-class HelloWorld {
+public class Main {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World");
+        Date currentDate = new Date();
+        System.out.println("Hello, World Date : " + currentDate);
     }
 }
